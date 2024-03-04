9b128adf3939478ff288e76c56573cdf0f711c23
mettemarie <mettemariebech@hotmail.com>
2024-03-04 14:43:34 +0100

    refactor: insert hardcoded data to MongoDB

--- app/db/seedingDb.server.js
+++ app/db/seedingDb.server.js
@@ -1,40 +1,64 @@
 import mongoose from "mongoose";
 
+// ========================== initData ========================== //
 export default async function seedDb() {
-  const entryCount = await mongoose.models.Entry.countDocuments();
-  if (entryCount === 0) {
+  const userCount = await mongoose.models.User.countDocuments();
+  const eventsCount = await mongoose.models.Events.countDocuments();
+  if (userCount === 0 || eventsCount === 0) {
     console.log("Seeding database...");
     insertData();
   }
 }
 
 async function insertData() {
-  const entries = [
+  const User = mongoose.models.User;
+  const Events = mongoose.models.Events;
+
+  console.log("Dropping collections...");
+  await User.collection.drop();
+  await Events.collection.drop();
+
+  console.log("Inserting data...");
+  
+  
+  // Insert users
+  const user1 = await User.create({
+    username: 'Anna Finsrud',
+    email: 'anna@kolle.com',
+    password: 'test123'
+  });
+
+  const user2 = await User.create({
+    username: 'Freja Lund Rasmussen',
+    email: 'freja@kolle.com',
+    password: 'test123'
+  });
+
+  const user3 = await User.create({
+    username: 'Mette-Marie Bech Sørensen',
+    email: 'MM@kolle.com',
+    password: 'test123'
+  });
+
+  // Opret et event
+  await Events.insertMany([
     {
-      date: new Date("2024-01-01"),
-      type: "work",
-      text: "I'm working",
+      titel: 'Børnefødselsdag',
+      description: 'Min 28-års fødselsdag fest',
+      time: '18:00',
+      date: new Date('2024-03-16'),
+      place: 'Min adresse',
+      userID: user3._id, // Brug ID'et fra en eksisterende bruger
+      attendees: [user1._id, user2._id, user3._id] // Tilføj ID'er for deltagere
     },
     {
-      date: new Date("2024-01-15"),
-      type: "learning",
-      text: "I'm learning",
-    },
-    {
-      date: new Date("2024-02-01"),
-      type: "interesting-thing",
-      text: "I'm doing something interesting",
-    },
-    {
-      date: new Date("2024-02-15"),
-      type: "learning",
-      text: "Remix Auth with FormStrategy and Post App",
-    },
-    {
-      date: new Date("2024-02-22"),
-      type: "work",
-      text: "Remix Work Journal",
-    },
-  ];
-  await mongoose.models.Entry.insertMany(entries);
+      titel: 'Sorring-tur',
+      description: 'Vi skal nusse lam',
+      time: '10:00',
+      date: new Date('2024-04-27'),
+      place: 'Sorring',
+      userID: user1._id, // Brug ID'et fra en eksisterende bruger
+      attendees: [user1._id, user2._id, user3._id] // Tilføj ID'er for deltagere
+    }
+  ]);
 }
