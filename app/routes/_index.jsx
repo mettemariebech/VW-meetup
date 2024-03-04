bc5cd2f02ad5491f5bc6d3f8aec9a68782881b97
mettemarie <mettemariebech@hotmail.com>
2024-03-04 14:46:13 +0100

    refactor: make simple landingpage

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,20 +1,25 @@
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import mongoose from "mongoose";
+// import { json } from "@remix-run/node";
+// import { useLoaderData } from "@remix-run/react";
+// import mongoose from "mongoose";
+import { Link } from "@remix-run/react";
 
-export async function loader() {
-  const entries = await mongoose.models.Entry.find({});
-  return json({ entries });
-}
+// export async function loader() {
+//   const events = await mongoose.models.Events.find({});
+//   return json({ entries: events });
+// }
 
 export default function Index() {
-  const { entries } = useLoaderData();
+  // const events = useLoaderData();
 
   return (
-    <div className="p-8 text-slate-50 bg-slate-900">
-      <code>
-        <pre className="animate-pulse">{JSON.stringify(entries, null, 2)}</pre>
-      </code>
+    <div class="wrapper">
+      <div class="content">
+        <button class="btn">Sign in</button>
+        <button class="btn">Sign up</button>
+        <Link to="/events" class="link">
+          Browse events..
+        </Link>
+      </div>
     </div>
   );
 }
