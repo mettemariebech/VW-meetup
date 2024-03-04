196f4307032fd0ec192a27b98e551cb6e2952578
mettemarie <mettemariebech@hotmail.com>
2024-03-04 15:47:34 +0100

    refactor: Install & Use Form Strategy

--- app/services/auth.server.jsx
+++ app/services/auth.server.jsx
@@ -1,6 +1,31 @@
 // app/services/auth.server.ts
-import { Authenticator } from "remix-auth";
+import { Authenticator, AuthorizationError } from "remix-auth";
 import { sessionStorage } from "./session.server";
+import { FormStrategy } from "remix-auth-form";
+
 // Create an instance of the authenticator, pass a generic with what
 // strategies will return and will store in the session
 export let authenticator = new Authenticator(sessionStorage);
+
+// Tell the Authenticator to use the form strategy
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    let mail = form.get("mail");
+    let password = form.get("password");
+    let user = null;
+
+    // login the user, this could be whatever process you want
+    if (mail === "race@eaaa.dk" && password === "test01") {
+      user = {
+        mail,
+      };
+
+      return user;
+    } else {
+      // if problem with user throw error AuthorizationError
+      throw new AuthorizationError("Bad Credentials");
+    }
+  }),
+
+  "user-pass",
+);
