ce38efd7b64c458e100a505a23da4601f06c6cc4
mettemarie <mettemariebech@hotmail.com>
2024-03-05 14:08:45 +0100

    Show all events

--- app/routes/events.jsx
+++ app/routes/events.jsx
@@ -1,7 +1,31 @@
+import { authenticator } from "~/services/auth.server";
+import mongoose from "mongoose";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ request }) {
+  await authenticator.isAuthenticated(request, {
+    failureRedirect: "/",
+  });
+
+  const events = await mongoose.models.Events.find().exec();
+  return json({ events });
+}
+
 export default function Events() {
+  const { events } = useLoaderData();
+
   return (
-    <div className="page">
+    <div className="events-container">
       <h1>Events</h1>
+      <ul className="events-list">
+        {events.map((event) => (
+          <li key={event._id} className="event-item">
+            <h2 className="event-title">{event.titel}</h2>
+            <p className="event-description">{event.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
