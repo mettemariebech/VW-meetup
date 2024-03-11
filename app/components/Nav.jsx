819bed48426d30e72702a731fefc4393f053cd64
mettemarie <mettemariebech@hotmail.com>
2024-03-11 10:23:44 +0100

    Style: Nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,10 +2,27 @@ import { NavLink } from "@remix-run/react";
 
 export default function Nav() {
   return (
-    <nav>
-      <NavLink to="/events">Events</NavLink>
-      <NavLink to="/add-event">Add Event</NavLink>
-      <NavLink to="/profile">Profile</NavLink>
+    <nav
+      className=" grid grid-cols-3"
+    >
+      <NavLink
+        to="/events"
+        className="inline-block text-center px-4 py-6 no-underline uppercase font-semibold text-lg tracking-wide rounded-lg transition duration-500 ease-in-out"
+      >
+        Events
+      </NavLink>
+      <NavLink
+        to="/add-event"
+        className="inline-block text-center px-4 py-6 no-underline uppercase font-semibold text-lg tracking-wide rounded-lg transition duration-500 ease-in-out"
+      >
+        Add Event
+      </NavLink>
+      <NavLink
+        to="/profile"
+        className="inline-block text-center px-4 py-6  uppercase font-semibold text-lg tracking-wide rounded-lg transition duration-500 ease-in-out"
+      >
+        Profile
+      </NavLink>
     </nav>
   );
 }
