06bb6c6b65a8fbcbcb230e7500ffa9ade5c9e033
mettemarie <mettemariebech@hotmail.com>
2024-03-11 11:31:22 +0100

    fix: nav-bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,21 +1,26 @@
 import { NavLink } from "@remix-run/react";
+import logo from "../images/VWMeetup-small-logo.webp";
 
 export default function Nav() {
   return (
-    <nav
-      className=" grid grid-cols-3"
-    >
+    <nav className=" grid grid-cols-3 bg-transparent absolute top-0 w-full z-10">
       <NavLink
         to="/events"
         className="inline-block text-center px-4 py-6 no-underline uppercase font-semibold text-lg tracking-wide rounded-lg transition duration-500 ease-in-out"
       >
         Events
       </NavLink>
-      <NavLink
+      {/* <NavLink
         to="/add-event"
         className="inline-block text-center px-4 py-6 no-underline uppercase font-semibold text-lg tracking-wide rounded-lg transition duration-500 ease-in-out"
       >
         Add Event
+      </NavLink> */}
+      <NavLink
+        to="/"
+        className="flex justify-center text-center px-4 py-6 no-underline uppercase font-semibold text-lg tracking-wide rounded-lg transition duration-500 ease-in-out "
+      >
+        <img src={logo} alt="Logo" className="w-11" />
       </NavLink>
       <NavLink
         to="/profile"
